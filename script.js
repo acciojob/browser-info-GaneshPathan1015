f7afbb4686c14fcbb0580f671e7383572dcd0b41
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-26 13:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let user = navigator.userAgent;
+let user2 =navigator.appName;
+
+let user3 = navigator.appVersion;
+
+
+
+const display = document.getElementById("browser-info");
+display.innerText = user+user2+user3;
